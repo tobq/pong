2f160597bdfbb44602abff7a4ebddca68ff7bf58
Tobi <Tobi.akiny@gmail.com>
2016-08-25 00:36:01 +0100

    ball moving up and down

--- pong/Form1.Designer.cs
+++ pong/Form1.Designer.cs
@@ -29,11 +29,54 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.paddle = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // paddle
+            // 
+            this.paddle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.paddle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.paddle.Location = new System.Drawing.Point(193, 421);
+            this.paddle.Name = "paddle";
+            this.paddle.Size = new System.Drawing.Size(200, 20);
+            this.paddle.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 16;
+            this.timer1.Tick += new System.EventHandler(this.frame);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ball.Location = new System.Drawing.Point(273, 127);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.paddle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Release);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label paddle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label ball;
     }
 }
 
--- pong/Form1.cs
+++ pong/Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace pong
 {
+    class Ball : Label
+    {
+
+    }
+
     public partial class Form1 : Form
     {
+        static bool lef = false;
+        static bool rig = false;
+        static int padspeed = 5;
+        static int ballspeed = 5;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void FormDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) lef = true;
+            if (e.KeyCode == Keys.Right) rig = true;
+        }
+
+        private void Release(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) lef = false;
+            if (e.KeyCode == Keys.Right) rig = false;
+        }
+
+        private void frame(object sender, EventArgs e)
+        {
+            Point loc = paddle.Location;
+            if (lef) loc = new Point(Math.Max(Math.Min(loc.X - padspeed, ClientRectangle.Width - paddle.Width), 0), ClientRectangle.Height - 50);
+            if (rig) loc = new Point(Math.Max(Math.Min(loc.X + padspeed, ClientRectangle.Width - paddle.Width), 0), ClientRectangle.Height - 50);
+            paddle.Location = loc;
+
+            if (ball.Location.Y <= 0 || ball.Location.Y >= paddle.Location.Y-ball.Height) ballspeed *= -1;
+            Text = ball.Location.Y.ToString()+" speed: "+ballspeed;
+            ball.Location = new Point(Math.Min(Math.Max(ball.Location.X,0),paddle.Location.Y), ball.Location.Y + ballspeed);
         }
     }
 }
